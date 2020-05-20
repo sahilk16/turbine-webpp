a21ef5d936e930606f30aa6a3ea96524f6aaee6b
ksahil16 <sahil.karkhanis@accenture.com>
2020-05-20 16:09:36 +0200

    going back to original to check

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 import pickle
-from sklearn.preprocessing import MinMaxScaler
 
 
 app = Flask(__name__)
-#model = pickle.load(open('randomForestRegressor.pkl','rb'))
-
-def scaling(data):
-    
-    sc = MinMaxScaler()
-    values = sc.transform(final_features)
-    return values
-
+model = pickle.load(open('randomForestRegressor.pkl','rb'))
 
 
 @app.route('/')
@@ -25,18 +17,11 @@ def home():
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    if scorekey is None:
-        headers = {'Content-Type':'application/json'}
-    else:
-        headers = {'Content-Type':'application/json', 'Authorization':('Bearer ' + scorekey)}
-
-    resp = requests.post(scoreurl, scaling(final_features), headers=headers)
-
-    prediction = resp
+    prediction = model.predict(final_features)
     print(prediction[0])
 
     #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="Output Power for the Turbine {}".format(prediction[0]))
+    return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
