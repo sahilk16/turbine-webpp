92e6e965dc7a33059305af3b29ed6003e97ef67d
ksahil16 <sahil.karkhanis@accenture.com>
2020-05-21 17:29:10 +0200

    changed the names accordingly

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('randomForestRegressor.pkl','rb'))
-
+#model = pickle.load(open('randomForestRegressor.pkl','rb'))
+    
 
 @app.route('/')
 def home():
